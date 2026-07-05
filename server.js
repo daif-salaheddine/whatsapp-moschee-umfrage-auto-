b92b7c87e76d196eeac530c74a427773b5ae2e9e
Daif, Salaheddine (External) <salaheddine.daif.external@epg.com>
2026-07-05 18:45:04 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const { Client, LocalAuth, Poll } = require('whatsapp-web.js');
+const express = require('express');
+const qrcode = require('qrcode-terminal');
+
+const app = express();
+app.use(express.json());
+
+const client = new Client({ authStrategy: new LocalAuth() });
+
+client.on('qr', qr => qrcode.generate(qr, { small: true }));
+client.on('ready', () => console.log('WhatsApp ready!'));
+
+app.post('/send', async (req, res) => {
+  const { groupId } = req.body;
+  const poll = new Poll(
+    'Wer ist nächsten Freitag dabei? (Freitagsgebet)',
+    ['Ich bin dabei ❤️', 'Leider nicht 😔'],
+    { allowMultipleAnswers: false }
+  );
+  await client.sendMessage(groupId, poll);
+  res.json({ success: true });
+});
+
+client.initialize();
+app.listen(3000, () => console.log('Server running on port 3000'));
