eeaa80793bc53542f95adc112e4d08840a9d4f74
Daif, Salaheddine (External) <salaheddine.daif.external@epg.com>
2026-07-06 13:29:17 +0200

    qr code updated

--- server.js
+++ server.js
@@ -1,28 +1,37 @@
 const { Client, LocalAuth, Poll } = require('whatsapp-web.js');
 const express = require('express');
-const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode');
 
 const app = express();
 app.use(express.json());
 
+let qrCodeData = '';
+
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-gpu',
-      '--no-zygote',
-      '--no-first-run',
-      '--disable-accelerated-2d-canvas',
-    ],
+    executablePath: '/usr/bin/chromium',
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
   }
 });
 
-client.on('qr', qr => qrcode.generate(qr, { small: true }));
-client.on('ready', () => console.log('WhatsApp ready!'));
+client.on('qr', async qr => {
+  qrCodeData = await qrcode.toDataURL(qr);
+  console.log('QR code generated');
+});
+
+client.on('ready', () => {
+  console.log('WhatsApp ready!');
+  qrCodeData = '';
+});
+
+app.get('/', (req, res) => {
+  if (qrCodeData) {
+    res.send(`<html><body><h2>Scan this QR code with WhatsApp</h2><img src="${qrCodeData}"/></body></html>`);
+  } else {
+    res.send('<html><body><h2>WhatsApp is connected!</h2></body></html>');
+  }
+});
 
 app.post('/send', async (req, res) => {
   const { groupId } = req.body;
